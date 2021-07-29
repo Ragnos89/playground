8687880ef2d2f0ae4fe0bd5cf76b92edfdd084e6
Ragnos89 <87934506+Ragnos89@users.noreply.github.com>
2021-07-29 14:00:23 +0200

    added filtering list for specifict produkct

--- arrays_spread.js
+++ arrays_spread.js
@@ -1,3 +1,9 @@
 const list1 = ['mleko','kawa'];
 const list2 = [...list1,'jajka'];
-console.log(list2);
+const filter = (product,list) => {
+    const templist = [...list];
+    return templist.filter(el => el === product);
+};
+//console.log(list2);
+console.log(filter('kawa',list2));
+
