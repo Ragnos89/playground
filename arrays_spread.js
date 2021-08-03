22988b81e596ccfbb2408d9da1b3b0ce3162762f
Ragnos89 <87934506+Ragnos89@users.noreply.github.com>
2021-08-03 17:34:52 +0200

    git commit -m "optimising the filter function"

--- arrays_spread.js
+++ arrays_spread.js
@@ -1,8 +1,7 @@
 const list1 = ['mleko','kawa'];
 const list2 = [...list1,'jajka'];
 const filter = (product,list) => {
-    const templist = [...list];
-    return templist.filter(el => el === product);
+    return list.filter(el => el === product);
 };
 //console.log(list2);
 console.log(filter('kawa',list2));
